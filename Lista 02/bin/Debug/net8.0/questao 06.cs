d7e077cc33093f9a5af439ddf51c49bca5b2cb7c
henriquetaite <henriquetaite@gmail.com>
2024-04-04 00:33:25 -0300

    Criando questões para lista 02

---
+++ Lista 02/bin/Debug/net8.0/questao 06.cs
@@ -0,0 +1,26 @@
+using System;
+class Program {
+    public static void Main () {
+
+//Declarando variáveis
+double A = 0, B = 0;
+
+//Solicitando que o usuário digite 2 números para comparação
+Console.WriteLine("Digite 2 números aleatórios");
+
+//Fazendo a leitura e registro dos números
+A = double.Parse (Console.ReadLine());
+B = double.Parse (Console.ReadLine());
+
+//Criando condições e definindo comandos para cada uma delas 
+    if (A > B) {
+    Console.WriteLine ("O maior número informado é: {0}", A);
+    }
+    else if (A < B) {
+    Console.WriteLine ("O maior número informado é: {0}", B);
+    }
+    else {
+    Console.WriteLine ("Os números possuem valores iguais");
+    }
+}
+}   
