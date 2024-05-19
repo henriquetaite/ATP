84f431a5a63e02909cea899c9b6cf9023c9e029c
henriquetaite <henriquetaite@gmail.com>
2024-05-18 21:26:13 -0300

    Fechando questão 5 Lista 4

--- Lista02/Lista04/obj/Debug/net8.0/Lista04.AssemblyInfo.cs
+++ Lista02/Lista04/obj/Debug/net8.0/Lista04.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lista04")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75b96bbf8f709e3a23dbc4fa653bc2d463d9cb06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a337ef3b18b953e9bb692716d55391a628fb427d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lista04")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lista04")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Lista04/questao05.cs
+++ Lista04/questao05.cs
@@ -1,8 +1,43 @@
 using System;
 public class Questao05
 {
-    public void Rodar()
+    public static void Rodar()
     {
-        Console.WriteLine("Olá, mundo!!!");
+//Declarando variáveis
+        int a, b, mdc;
+
+//Solicitando números ao usuário
+        Console.WriteLine ("Digite 2 números para saber o MDC");
+        a = int.Parse (Console.ReadLine());
+        b = int.Parse (Console.ReadLine());
+
+//Acionando a função para retornar o MDC
+        mdc = CalcularMdc (a, b);
+
+//Exibiindo o resultado MDC
+        Console.WriteLine ("O MDC dos números que você digitou é: {0}", mdc);
+    }
+
+//Criando função para retornar o mdc
+        public static int CalcularMdc (int a, int b) {
+
+//Criando método do while para fazer as operações até chegar ao MDC
+        do {
+            if (a > b) {
+                a = a - b;
+                b = a;
+            }
+            else if (a < b) {
+                b = b - a;
+                a = b;
+            }
+        }
+        while (a != b);
+
+//Igualando mdc a sua variável correta
+        int mdc = a;
+
+//Informando o MDC no return da função
+        return mdc;
     }
 }
