85c8b6534cd94d46d67b699478df9f13b6a182cf
henriquetaite <henriquetaite@gmail.com>
2024-05-20 15:02:42 -0300

    Finalizando questão 9 da Lista 4

--- Lista04/questao09.cs
+++ Lista04/questao09.cs
@@ -1,8 +1,39 @@
 using System;
 public class Questao09
 {
-    public void Rodar()
+    public static void Rodar()
     {
-        Console.WriteLine("Olá, mundo!!!");
+//Declarando variáveis: total (total de alunos) e soma (soma das notas entre 6 e 10)
+        double nota = 0, media = 0, soma = 0;
+        int total = 0;
+
+//Criando comando do while para registrar várias notas
+       do {
+
+//Solicitando notas ao usuário
+            Console.WriteLine ("Digite as notas dos alunos ou um número fora do intervalo 0 a 10 para parar");
+            nota = double.Parse (Console.ReadLine());
+
+//Criando comando if para ler e calcular somente notas entre 6 e 10
+        if (nota >= 6 && nota <= 10) {
+            soma += nota;
+            total++;
+        }
+//Criando condicional para acionar a função quando um valor fora do intervalo for digitado
+            if (nota < 0 || nota > 10) {
+                media = CalcularMedia (nota, media, total, soma);
+                Console.WriteLine ("A média dos alunos que obtiveram notas entre 6 e 10 é: {0}", media);
+            }
+       }
+        while (nota >= 0 && nota <= 10);
+    }
+//Criando função para calcular média das notas maiores que 6
+    public static double CalcularMedia (double nota, double media, int total, double soma) {
+
+//Fazendo cálculo da média
+        media = soma / total;
+
+//Fazendo comando return para retornar o valor da média 
+        return media;
     }
 }
