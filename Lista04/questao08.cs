6c60ce4c78dfa0ed6290a3e3e60f147a9039fec1
henriquetaite <henriquetaite@gmail.com>
2024-05-20 13:31:29 -0300

    Finalizando questão 8 da lista 4

--- Lista04/questao08.cs
+++ Lista04/questao08.cs
@@ -1,8 +1,38 @@
 using System;
 public class Questao08
 {
-    public void Rodar()
+    public static void Rodar()
     {
-        Console.WriteLine("Olá, mundo!!!");
+//Declarando variáveis
+        int N;
+        double S = 0;
+
+//Solicitando e registrando N ao usuário
+        Console.WriteLine ("Informe um valor inteiro positivo");
+        N = int.Parse (Console.ReadLine());
+
+//Acionando função
+            S = CalcularFuncao (N, S);
+
+//Imprimindo resultado de S
+            Console.WriteLine ("O valor de S em função de N é: {0}", S);
+
+//Criando comando if para invalidar valores que não sejam positivos
+        if (N <= 0) {
+            Console.WriteLine ("Valor inválido! Digite outro");
+            N = int.Parse (Console.ReadLine());
+        }
+    }   
+//Criando função para retornar S
+        public static double CalcularFuncao (int N, double S) {
+
+//Criando comando for para fazer cálculos até N
+            for (int n = 2; n <= N; n++) {
+                double numerador = Math.Pow (N,2) + 1;
+                double denominador = N + 3;
+                S += numerador/denominador;
+            }
+//Fazendo return para retornar valor final de S
+                return S;
+        }
     }
-}
