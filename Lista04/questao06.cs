06353f01b222270477eb18fee6af7decc3121e43
henriquetaite <henriquetaite@gmail.com>
2024-05-19 21:48:58 -0300

    Finalizando questão 6 da Lista 4

--- Lista04/questao06.cs
+++ Lista04/questao06.cs
@@ -1,8 +1,40 @@
 using System;
 public class Questao06
 {
-    public void Rodar()
+    public static void Rodar()
     {
-        Console.WriteLine("Olá, mundo!!!");
+//Declarando variáveis
+        int x;
+        bool ehPositivo;
+        string opcao;
+
+//Criando método do while para ler N números e acionar o procedimento para cada um
+        do {
+//Solicitando e registrando o número inteiro do usuário
+        Console.WriteLine ("Digite um número inteiro");
+        x = int.Parse (Console.ReadLine());
+
+//Acionando procedimento para identificar se é positivo ou negativo
+        ehPositivo = VerificarNumero (x);
+
+//Imprimindo respostas a partir de condições
+            if (ehPositivo) {
+                Console.WriteLine ("O número que você digitou é: positivo");
+            }
+            else {
+                Console.WriteLine ("O número que você digitou é: negativo");
+            }
+
+//Perguntando se deseja digitar mais números
+        Console.WriteLine ("Deseja digitar mais um número? Digite S para Sim e outra tecla para Não");
+        opcao = Console.ReadLine ();
+    }
+    while (opcao == "s" || opcao == "S");
+    }
+//Criando função para receber o número e definir se é positivo ou negativo
+        public static bool VerificarNumero (int x) {
+
+//Criando return para mostrar se é positivo ou não
+        return x > 0;
     }
 }
