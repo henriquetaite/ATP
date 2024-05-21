feb9861b379fb0be2f8ed8e52d2098d289118436
henriquetaite <henriquetaite@gmail.com>
2024-05-21 00:23:50 -0300

    Finalizando questão 1 da Lista 5

--- Lista05/Questao01.cs
+++ Lista05/Questao01.cs
@@ -14,5 +14,21 @@ public class Questao01 {
 //Usando função .Next para preencher cada posição do vetor com um número aleatório
             N[i] = randNum.NextDouble();
         }
+
+//Inicializando variável M com o valor do primeiro elemento do vetor N
+                double M = N[0];
+                int P = 0;
+
+//Criando comando for para percorrer o vetor 
+        for (int i = 1; i < 20; i++) { //i inicializado na posição 1 porque double M já foi declarado como na posição 0, não havendo necessidade de percorre-la novamente
+
+//Criando comando if para atualizar a variável M
+                if (N[i] < M) {
+                        M = N[i];
+                        P = i;
+                }
+        }
+//Imprimindo resultados do enunciado
+        Console.WriteLine ("O menor elemento de N é " + M + " e sua posição dentro do vetor é: " + P); 
     }
 }
