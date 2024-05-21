fcd210e6dab2b2d7b42b2dd39a6d3524630a9ad1
henriquetaite <henriquetaite@gmail.com>
2024-05-21 15:24:18 -0300

    Finalizando questão 2 da lista 5

--- Lista05/Questao02.cs
+++ Lista05/Questao02.cs
@@ -9,16 +9,43 @@ public class Questao02 {
         PreencherNotas (vetorNotas);
 
 //Acionando procedimento que faz os cálculos solicitados
-        CalcularNotas (vetorNotas, nota, soma, media);
-
-//Criando procedimento que vai preencher o vetor com as Notas
-        public static void PreencherNotas (double nota, double [] vetorNotas){
-
-        }
-//Criando procedimento que vai fazer os cálculos e contagem solicitados
-        public static void CalcularNotas (double [] vetorNotas, double nota, double soma, double media) {
-            
-        }
-
+        CalcularNotas (vetorNotas);
     }
-}
+//Criando procedimento que vai preencher o vetor com as Notas
+        public static void PreencherNotas (double [] vetorNotas) {
+
+//Solicitando que o usuário preencha o vetor com as 10 notas
+            Console.WriteLine ("Digite as notas dos 10 alunos");
+
+//Criando comando for para armazenar as notas no vetor
+            for (int i = 0; i < 10; i++) {
+                Console.Write ("Digite a nota {0}:", i + 1);
+                vetorNotas [i] = double.Parse (Console.ReadLine());
+            }
+        }
+//Criando procedimento que vai fazer os cálculos e contagem solicitados (acima = quantidade de alunos acima da média)
+        public static void CalcularNotas (double [] vetorNotas) {
+
+//Declarando variáveis necessárias para cálculos
+            double soma = 0, media = 0;
+            int acima = 0;
+
+//Fazendo comando for para contar os alunos acima da média
+            for (int i = 0; i < 10; i++) {
+                soma += vetorNotas [i];
+            }
+//Calculando a média
+            media = (soma / 10);
+
+//Criando comando for para fazer demais cálculos
+            for (int i = 0; i < 10; i++) {
+
+//Criando comando if para contar um aluno, se ele estiver acima da média
+                if (vetorNotas [i] > media) {
+                    acima++;
+                }
+            }
+//Imprimindo resultados solicitados
+            Console.WriteLine ("A média da turma é " + media + " e {0} alunos estão acima dela", acima);
+        }
+    }
