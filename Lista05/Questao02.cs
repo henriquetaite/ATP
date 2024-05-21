8e19bfe45117491f908fda9a6e8e55e2c78d9165
henriquetaite <henriquetaite@gmail.com>
2024-05-21 11:34:56 -0300

    Iniciando questão 2 da lista 5

--- Lista05/Questao02.cs
+++ Lista05/Questao02.cs
@@ -2,7 +2,23 @@ using System;
 public class Questao02 {
     public static void Rodar() {
 
+//Declarando e inicializando vetor que será usado em 2 procedimentos diferentes
+        double [] vetorNotas = new double [10];
 
+//Acionando procedimento que preenche o vetor
+        PreencherNotas (vetorNotas);
+
+//Acionando procedimento que faz os cálculos solicitados
+        CalcularNotas (vetorNotas, nota, soma, media);
+
+//Criando procedimento que vai preencher o vetor com as Notas
+        public static void PreencherNotas (double nota, double [] vetorNotas){
+
+        }
+//Criando procedimento que vai fazer os cálculos e contagem solicitados
+        public static void CalcularNotas (double [] vetorNotas, double nota, double soma, double media) {
+            
+        }
 
     }
 }
