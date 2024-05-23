a64c51d2f520672e1d50f84d7d7b868c187510f2
henriquetaite <henriquetaite@gmail.com>
2024-05-22 22:25:29 -0300

    lista 04

--- Lista05/Program.cs
+++ Lista05/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o número da questão que deseja executar (1-10):");
+        int questao = int.Parse(Console.ReadLine());
+
+        switch (questao)
+        {
+            case 1:
+                Questao01 questao01 = new Questao01();
+                questao01.Rodar();
+                break;
+            case 2:
+                Questao02 questao02 = new Questao02();
+                questao02.Rodar();
+                break;
+            case 3:
+                Questao03 questao03 = new Questao03();
+                questao03.Rodar();
+                break;
+            case 4:
+                Questao04 questao04 = new Questao04();
+                questao04.Rodar();
+                break;
+            case 5:
+                Questao05 questao05 = new Questao05();
+                questao05.Rodar();
+                break;
+            case 6:
+                Questao06 questao06 = new Questao06();
+                questao06.Rodar();
+                break;
+            case 7:
+                Questao07 questao07 = new Questao07();
+                questao07.Rodar();
+                break;
+            case 8:
+                Questao08 questao08 = new Questao08();
+                questao08.Rodar();
+                break;
+            case 9:
+                Questao09 questao09 = new Questao09();
+                questao09.Rodar();
+                break;
+            case 10:
+                Questao10 questao10 = new Questao10();
+                questao10.Rodar();
+                break;
+            default:
+                Console.WriteLine("Questão inválida.");
+                break;
+        }
+    }
+}
