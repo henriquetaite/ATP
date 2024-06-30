bc05c6060651859aad5d446e428881baff13d00f
henriquetaite <henriquetaite@gmail.com>
2024-06-30 13:31:11 -0300

    Primeira tentativa para rodar questão 01 da lista 06

--- Lista06/Questao01.cs
+++ Lista06/Questao01.cs
@@ -2,33 +2,17 @@ using System;
 public class Questao01 {
     public void Rodar() {
 
-//Declarando vetor N com 20 posições
-        double [] N = new double [20];
+// Solicitando frase ao usuário
+        Console.WriteLine("Digite uma frase:");
+        string frase = Console.ReadLine();
+        int contadorEspacos = 0;
 
-//Gerando números aleatórios para preencher o vetor
-        Random randNum = new Random();
-
-//Definindo o tamanho do vetor com suas posições
-        for (int i = 0; i < 20; i++) {
-
-//Usando função .Next para preencher cada posição do vetor com um número aleatório
-            N[i] = randNum.NextDouble();
+// Percorrendo cada caracter para contar os espaços em branco
+        foreach (char c in frase) {
+            if (c == ' ') {
+                contadorEspacos++;
+            }
         }
-
-//Inicializando variável M com o valor do primeiro elemento do vetor N
-                double M = N[0];
-                int P = 0;
-
-//Criando comando for para percorrer o vetor 
-        for (int i = 1; i < 20; i++) { //i inicializado na posição 1 porque double M já foi declarado como na posição 0, não havendo necessidade de percorre-la novamente
-
-//Criando comando if para atualizar a variável M
-                if (N[i] < M) {
-                        M = N[i];
-                        P = i;
-                }
-        }
-//Imprimindo resultados do enunciado
-        Console.WriteLine ("O menor elemento de N é " + M + " e sua posição dentro do vetor é: " + P); 
+        Console.WriteLine("Número de espaços em branco na frase: " + contadorEspacos);
     }
 }
