31b8fc318f8e77ac1bd4c3010788b199dd43a816
henriquetaite <henriquetaite@gmail.com>
2024-07-01 22:03:21 -0300

    Testando questão 07 da lista 06

--- obj/Debug/net8.0/ATP.AssemblyInfo.cs
+++ obj/Debug/net8.0/ATP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cd0f248f9c53127b3e796226be6f04eb13e6e5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+368c34fde64340c9ddc257726eb16f61239aae51")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
