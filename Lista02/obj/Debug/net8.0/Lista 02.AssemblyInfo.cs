077ce8db5c7ae3a36d7000b9f631fe0c705d64ce
henriquetaite <henriquetaite@gmail.com>
2024-07-01 22:08:48 -0300

    Aceitando últimos commits Lista 02

--- Lista02/obj/Debug/net8.0/Lista 02.AssemblyInfo.cs
+++ Lista02/obj/Debug/net8.0/Lista 02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lista 02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cd0f248f9c53127b3e796226be6f04eb13e6e5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31b8fc318f8e77ac1bd4c3010788b199dd43a816")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lista 02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lista 02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
